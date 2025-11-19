f3247fc4dfa7c26183033e8d6dd9d718b9ab67d3
Logan Faraud <logan.faraud@gmail.com>
2025-11-19 21:57:49 +0100

    Add dynamic style - End of part 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,33 +12,36 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.scss'
 })
 export class App implements OnInit{
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap (
+    this.faceSnaps = [
+      new FaceSnap (
       'My first FaceSnap',
       'Just a simple description for my first FaceSnap!',
       new Date(),
-      5,
+      15,
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
-    );
-
-    this.myOtherSnap = new FaceSnap (
-      'My other FaceSnap',
-      'Just a simple description for my other FaceSnap!',
-      new Date(),
-      5,
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/First_drop_Black_Mamba_Phantasialand.JPG/250px-First_drop_Black_Mamba_Phantasialand.JPG'
-    );
-
-    this.myLastSnap = new FaceSnap (
-      'My last FaceSnap',
-      'Just a simple description for my last FaceSnap!',
-      new Date(),
-      5,
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Thompson_Peak_Idaho.JPG/330px-Thompson_Peak_Idaho.JPG'
-    );
+      ),
+      new FaceSnap (
+        'My other FaceSnap',
+        'Just a simple description for my other FaceSnap!',
+        new Date(),
+        100,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/First_drop_Black_Mamba_Phantasialand.JPG/250px-First_drop_Black_Mamba_Phantasialand.JPG'
+      ),
+      new FaceSnap (
+        'My last FaceSnap',
+        'Just a simple description for my last FaceSnap!',
+        new Date(),
+        4,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Thompson_Peak_Idaho.JPG/330px-Thompson_Peak_Idaho.JPG'
+      )
+    ];
+    this.faceSnaps[1].setLocation('Phantasialand, Germany');
   }
 }
--- src/app/models/face-snap.ts
+++ src/app/models/face-snap.ts
@@ -1,4 +1,5 @@
 export class FaceSnap {
+    location?: string;
     constructor(
         public title: string,
         public description: string,
@@ -13,5 +14,9 @@ export class FaceSnap {
 
     removeSnap(): void {
         this.snaps--;
+    }
+
+    setLocation(location: string): void {
+        this.location = location;
     }
 }
