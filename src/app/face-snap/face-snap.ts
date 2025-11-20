6ed0adf6f86100c7c08af49c4e2ce4e9893c4dbb
Logan Faraud <logan.faraud@gmail.com>
2025-11-20 13:27:15 +0100

    Add first pipes - End of part 4

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { NgClass, NgStyle } from '@angular/common';
+import {DatePipe, NgClass, NgStyle, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   imports: [
     NgStyle,
-    NgClass
+    NgClass,
+    UpperCasePipe,
+    DatePipe
   ],
   templateUrl: './face-snap.html',
   styleUrl: './face-snap.scss',
