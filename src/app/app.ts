ef41894a5a80106429a352a23cecd80f60a713c6
Logan Faraud <logan.faraud@gmail.com>
2025-11-21 17:25:56 +0100

    First Observable - Beginning part 1 of second course

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Header} from './header/header';
 import {RouterOutlet} from '@angular/router';
+import {interval, map, Observable} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     Header,
-    RouterOutlet
+    RouterOutlet,
+    AsyncPipe
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App{
+export class App implements OnInit {
+  interval$!: Observable<number>;
 
+  ngOnInit() {
+    this.interval$ = interval(1000);
+  }
 }
