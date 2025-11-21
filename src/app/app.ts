4eb60a03178e327eb09264d001d50675fc40c0ae
Logan Faraud <logan.faraud@gmail.com>
2025-11-21 17:47:25 +0100

    Play with high level observable operator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {Header} from './header/header';
 import {RouterOutlet} from '@angular/router';
-import {interval, map, Observable} from 'rxjs';
+import {
+  filter,
+  interval,
+  tap,
+  map,
+  Observable,
+  take,
+  mergeMap,
+  of,
+  delay,
+  concatMap,
+  exhaustMap,
+  switchMap
+} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
 
 @Component({
@@ -15,11 +28,30 @@ import {AsyncPipe} from '@angular/common';
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
-  interval$!: Observable<string>;
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
   ngOnInit() {
-    this.interval$ = interval(1000).pipe(
-      map((value) => value % 2 === 0 ? `${value} --> Pair`: `${value} --> Impair`)
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      switchMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
     );
+  }
+
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
   }
 }
