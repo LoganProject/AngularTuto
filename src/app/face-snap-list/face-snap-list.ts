cb4867c85c5a1f29feea78dc9658054f935d09d2
Logan Faraud <logan.faraud@gmail.com>
2025-11-21 18:08:42 +0100

    Use of a Subject to destroy an observale at component destruction - End of part 1

--- src/app/face-snap-list/face-snap-list.ts
+++ src/app/face-snap-list/face-snap-list.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FaceSnapComponent} from '../face-snap/face-snap';
 import {FaceSnap} from '../models/face-snap';
 import {FaceSnapsService} from '../services/face-snaps.service';
+import {interval, Subject, takeUntil, tap} from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -11,12 +12,23 @@ import {FaceSnapsService} from '../services/face-snaps.service';
   templateUrl: './face-snap-list.html',
   styleUrl: './face-snap-list.scss',
 })
-export class FaceSnapList implements OnInit {
+export class FaceSnapList implements OnInit, OnDestroy {
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getFaceSnaps();
+    this.destroy$ = new Subject<boolean>()
+
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
